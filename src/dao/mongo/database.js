9bec0d63a9178ee8244e7a60ed2ecf6243894e48
davidisnardi <davidisnardi38@gmail.com>
2024-01-31 21:59:31 -0300

    SegundaPreEntregaIsnardi

---
+++ src/dao/mongo/database.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const conectarBD = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://davidisnardi38:ffrz6Cl6uOIi8SOK@cluster0.wazftbi.mongodb.net/ecommerce?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Conexión a la base de datos establecida correctamente');
+  } catch (error) {
+    console.error('Error al conectar a la base de datos:', error.message);
+  }
+};
+
+export default conectarBD;
---
+++ src/routers/views.router.js
@@ -0,0 +1,65 @@
+import { Router } from "express"
+import ProductManager from '../dao/fsManagers/ProductManager.js'
+import { productModel } from "../dao/GeneralModels/GeneralModels.js";
+import { cartModel } from "../dao/GeneralModels/GeneralModels.js";
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+    try {
+      //const products = await ProductModel.find().lean().exec();
+      let pageNum = parseInt(req.query.page) || 1;
+      let itemsPorPage = parseInt(req.query.limit) || 10;
+      const products = await productModel.paginate({}, { page: pageNum , limit: itemsPorPage , lean:true });
+
+      products.prevLink = products.hasPrevPage ? `/products?limit=${itemsPorPage}&page=${products.prevPage}` : '';
+      products.nextLink = products.hasNextPage ? `/products?limit=${itemsPorPage}&page=${products.nextPage}` : '';
+      
+
+      console.log(products);
+      
+      res.render('home', products);
+    } catch (error) {
+      console.log('Error al leer los productos:', error);
+      res.status(500).json({ error: 'Error al leer los productos' });
+    }
+  });
+  
+  router.get('/realtimeproducts', async (req, res) => {
+    try {
+      const products = await productModel.find().lean().exec();
+      res.render('realTimeProducts', { products });
+    } catch (error) {
+      console.log('Error al leer los productos en tiempo real:', error);
+      res.status(500).json({ error: 'Error al leer los productos en tiempo real' });
+    }
+  });
+
+  router.get('/:cid', async (req, res) => {
+    try {
+      const id = req.params.cid
+      const result = await ProductModel.findById(id).lean().exec();
+      if (result === null) {
+        return res.status(404).json({ status: 'error', error: 'Product not found' });
+      }
+      res.render('productDetail', result);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al leer los productos' });
+    }
+  })
+
+  router.get('/carts/:cid', async (req, res) => {
+    // ID del carrito: 64a36d28ae5981f3f6e4488e
+    try {
+      const id = req.params.cid
+      const result = await cartModel.findById(id).lean().exec();
+      if (result === null) {
+        return res.status(404).json({ status: 'error', error: 'Cart not found' });
+      }
+      res.render('carts', { cid: result._id, products: result.products });
+    } catch (error) {
+      res.status(500).json({ status: 'error', error: error.message });
+    }
+  })
+  
+  export default router;
